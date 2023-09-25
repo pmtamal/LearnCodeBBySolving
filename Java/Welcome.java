99dfcbfdb3b08cb83b697adf26f6414b79ddd789
parvez <parvezmahmud@csebuet.org>
2023-09-25 09:17:15 +0600

    Intial commit of LearnCodeByProblemSolving

---
+++ Java/Welcome.java
@@ -0,0 +1,11 @@
+package LearnBySolving.Java;
+
+/**
+ * Welcome
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the world of coding......");
+    }
+}
